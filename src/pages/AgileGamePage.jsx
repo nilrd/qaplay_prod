0ebafc586846a263093ebc7492d518285d8164f2
Nilson Brites <nilson@qaplay.com.br>
2025-08-19 15:58:33 -0400

    Fix: Corrigido erro de importação em AgileGamePage.jsx após remoção de BDDGame

--- src/pages/AgileGamePage.jsx
+++ src/pages/AgileGamePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import { ArrowLeft } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import BDDGame from '../games/BDDGame';
+
 
 const AgileGamePage = () => {
   return (
@@ -20,7 +20,7 @@ const AgileGamePage = () => {
         </div>
       </div>
       
-      <BDDGame />
+
     </div>
   );
 };
